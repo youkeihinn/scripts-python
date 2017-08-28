5b396da48bf09d0b4d5ef6bcd85f38a63bc42c01
khy <yqb813@gmail.com>
2017-08-28 18:37:35 +0800

    add files

---
+++ flask-test/flaskr/flaskr.py
@@ -0,0 +1,10 @@
+#!coding:utf-8
+
+import sqlite3
+from flask import Flask,request,session,g,redirect,url_for,abort,render_template,flash
+
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
