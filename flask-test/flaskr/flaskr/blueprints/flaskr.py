38226c25958e4680ba41ae4ebc26f9bc0d8e05ed
khy <yqb813@gmail.com>
2017-08-30 15:09:53 +0800

    add templates and style

--- flask-test/flaskr/flaskr/blueprints/flaskr.py
+++ flask-test/flaskr/flaskr/blueprints/flaskr.py
@@ -34,7 +34,7 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('insert into entries (title,text) values(?.?)',[request.form['title'],reques.form['text']])
+    db.execute('insert into entries (title,text) values(?,?)',[request.form['title'],reques.form['text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('flaskr.show_entries'))
