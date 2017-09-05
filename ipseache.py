9efa2f17e8c0b1c425c2e234d8c59d116f51fa60
khy <yqb813@gmail.com>
2017-09-05 14:40:03 +0800

    添加ipseache脚本

---
+++ ipseache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#coding:utf-8
+#检查日志文本中ip出现次数
+
+import re,time
+def cdn_log(file_path):
+    global count
+    log = open(file_path,'r')
+    C = r'\.'.join([r'\d'{1,3}*4])
+    find = re.compile(C)
+    count = {}
+
+    for i in log:
+        for ip in find.findall(i):
+            count[ip] = count.get(ip,1)+1
+
+if __name__ == '__main__':
+    print time.clock()
+    num = 0
+    cdn_log('xxx')
+    R = count.items()
+    for i in R:
+        if i[1] > 0:
+            print i
+            num += 1
+    print '符合要求数量：%s耗时(%s)' % (num,time.clock())
